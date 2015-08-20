7205482d1672c07fa6c8152653625b44950530a1
david breetzke <u12056503@tuks.co.za>
2015-08-20 08:21:03 +0200

    More SQLite extension

--- group-chat-src/org/linphone/groupchat/storage/GroupChatStorageAndroidImpl.java
+++ group-chat-src/org/linphone/groupchat/storage/GroupChatStorageAndroidImpl.java
@@ -1,8 +1,11 @@
 package org.linphone.groupchat.storage;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import org.linphone.groupchat.interfaces.GroupChatStorage;
+
+import java.lang.String;
 
 /**
  *
@@ -13,5 +16,19 @@ import org.linphone.groupchat.interfaces.GroupChatStorage;
 
 public class GroupChatStorageAndroidImpl implements GroupChatStorage {
 
-    private class GroupChatHelper extends SQLiteOpenHelper{} // this class implements a class provided by the android sdk, SQLiteOpenHelper
+    //http://androidhive.info/2011/11/android-sqlite-database-tutorial is helpful
+    private class GroupChatHelper extends SQLiteOpenHelper{// this class implements a class provided by the android sdk, SQLiteOpenHelper
+        private static final int GROUPCHAT_DB_VERSION = 1;
+
+        private static final String GROUPCHAT_DB_NAME = "GroupChatStorageDatabase";
+
+        //add tables and name them
+
+        public GroupChatHelper(Context context){
+            super(context, GROUPCHAT_DB_NAME, null, GROUPCHAT_DB_VERSION);
+        }
+
+
+
+    }
 }
