ccfdab160f9e98a681ad74c8ae7c0728f543f492
izakblom <u13126777@tuks.co.za>
2015-08-22 21:42:27 +0200

    Add UI development project
    Jessica, import this project into eclipse. We can develop the UI here
    until we integrate with the core functionality. That way we can build
    and emulate from windows.

---
+++ gcUIDev/src/com/example/groupchatui/GroupChatActivity.java
@@ -0,0 +1,41 @@
+package com.example.groupchatui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public class GroupChatActivity extends FragmentActivity
+{
+	private static final String GROUP_CHAT_FRAGMENT = "";
+	private GroupChatMessagingFragment groupChatFragment;
+	private GroupChatSettingsFragment groupChatSettingsFragment;
+	
+	@Override
+	/*
+	 * Please modify as needed. Just included code that might be needed here...
+	 */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.groupchat_activity);
+		
+		Bundle extras = new Bundle();
+		// extras.putString("SipUri", getIntent().getExtras().getString("SipUri"));
+		
+		
+		GroupChatMessagingFragment gFragment = new GroupChatMessagingFragment();
+		//gFragment.setArguments(extras);
+		getSupportFragmentManager().beginTransaction().add(R.id.gcactivity, gFragment, "GroupChatFragment").commit();
+		
+		FragmentManager fm = getSupportFragmentManager();
+		groupChatFragment = (GroupChatMessagingFragment) fm.findFragmentByTag("GroupChatFragment");
+
+	    // If the Fragment is non-null, then it is currently being
+	    // retained across a configuration change.
+		if (groupChatFragment == null) {
+			groupChatFragment = new GroupChatMessagingFragment();
+			groupChatFragment.setArguments(extras);
+			fm.beginTransaction().add(R.id.gcactivity, groupChatFragment, "GroupChatFragment").commit();
+	    }
+	}
+}
---
+++ gcUIDev/src/com/example/groupchatui/GroupChatSettingsFragment.java
@@ -0,0 +1,117 @@
+package com.example.groupchatui;
+
+
+import android.widget.TextView;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+//import org.linphone.core.LinphoneBuffer;
+//import org.linphone.core.LinphoneChatMessage;
+//import org.linphone.core.LinphoneChatMessage.State;
+//import org.linphone.core.LinphoneContent;
+//import org.linphone.core.LinphoneCoreListenerBase;
+//import org.linphone.groupchat.core.LinphoneGroupChatRoom;
+//import org.linphone.groupchat.interfaces.GroupChatStorage;
+//import org.linphone.core.LinphoneChatMessage.LinphoneChatMessageListener;
+
+public class GroupChatSettingsFragment extends Fragment implements OnClickListener//, LinphoneChatMessageListener
+{
+//	private LinphoneGroupChatRoom groupChatRoom;
+//	private LinphoneCoreListenerBase mListener; 	// Really needed?
+//	private GroupChatStorage groupChatStorage;
+	private String sipUri;
+	private String displayName;
+	private String pictureUri;	
+	
+	private LinearLayout topBar;
+	private TextView back;
+	private EditText groupName;
+	private TextView groupParticipants;
+	private TextView addParticipant;
+	private TextView removeParticipant;
+	private TextView leaveGroup;
+	private static GroupChatSettingsFragment instance;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		instance = this;
+		return null;
+	}
+	
+	public void onPause()
+	{
+		
+		
+	}
+	
+	public void displayGroupName()
+	{
+		
+	}
+	
+	public void displayGroupMembers()
+	{
+		
+	}
+	
+	public void leaveGroup()
+	{
+		
+	}
+	
+	public void addMember()
+	{
+		
+	}
+	
+	
+	public void removeMember()
+	{
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+//	@Override
+//	public void onLinphoneChatMessageStateChanged(LinphoneChatMessage msg,
+//			State state) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void onLinphoneChatMessageFileTransferReceived(
+//			LinphoneChatMessage msg, LinphoneContent content,
+//			LinphoneBuffer buffer) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void onLinphoneChatMessageFileTransferSent(LinphoneChatMessage msg,
+//			LinphoneContent content, int offset, int size,
+//			LinphoneBuffer bufferToFill) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void onLinphoneChatMessageFileTransferProgressChanged(
+//			LinphoneChatMessage msg, LinphoneContent content, int offset,
+//			int total) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+}
