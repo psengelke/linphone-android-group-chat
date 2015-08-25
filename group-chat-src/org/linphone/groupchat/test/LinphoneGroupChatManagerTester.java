c71e1e53877935caba9f8b64d7ba1adba2f396d1
Paul Engelke <psengelke001@gmail.com>
2015-08-25 09:32:33 +0200

    Attempting to set up unit tests.

---
+++ group-chat-src/org/linphone/groupchat/test/LinphoneGroupChatManagerTester.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package org.linphone.groupchat.test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Paul
+ *
+ */
+public class LinphoneGroupChatManagerTester extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public LinphoneGroupChatManagerTester(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link org.linphone.groupchat.core.LinphoneGroupChatManager#createGroupChat(java.lang.String, org.linphone.core.LinphoneAddress, java.util.LinkedList, org.linphone.groupchat.interfaces.EncryptionHandler.EncryptionType, boolean)}.
+	 */
+	public void testCreateGroupChat() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.linphone.groupchat.core.LinphoneGroupChatManager#getGroupChat(java.lang.String)}.
+	 */
+	public void testGetGroupChat() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.linphone.groupchat.core.LinphoneGroupChatManager#deleteGroupChat(java.lang.String)}.
+	 */
+	public void testDeleteGroupChat() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.linphone.groupchat.core.LinphoneGroupChatManager#getGroupChatList()}.
+	 */
+	public void testGetGroupChatList() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.linphone.groupchat.core.LinphoneGroupChatManager#handleMessage(org.linphone.core.LinphoneCore, org.linphone.core.LinphoneChatRoom, org.linphone.core.LinphoneChatMessage)}.
+	 */
+	public void testHandleMessage() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.linphone.groupchat.core.LinphoneGroupChatManager#getInstance()}.
+	 */
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+}
