10111f5d22756f5652b202b3aa5be92257d3afee
izakblom <u13126777@tuks.co.za>
2015-09-30 19:51:09 +0200

    Revert "Retry project merge"
    This reverts commit 2a145cd0d176e29a9922b7a01eee6d0814de1be5.

---
+++ group-chat-src/org/linphone/groupchat/ui/GroupChatActivity.java
@@ -0,0 +1,40 @@
+package org.linphone.groupchat.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public class GroupChatActivity extends FragmentActivity
+{
+	private static final String GROUP_CHAT_FRAGMENT;
+	private GroupChatMessagingFragment groupChatFragment;
+	private GroupChatSettingsFragment groupChatSettingsFragment;
+	
+	@Override
+	/*
+	 * Please modify as needed. Just included code that might be needed here...
+	 */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.groupchat_activity);
+		
+		Bundle extras = new Bundle();
+		// extras.putString("SipUri", getIntent().getExtras().getString("SipUri"));
+		
+		
+		GroupChatMessagingFragment gFragment = new GroupChatMessagingFragment();
+		gFragment.setArguments(extras);
+		getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, gFragment, "GroupChatFragment").commit();
+		
+		FragmentManager fm = getSupportFragmentManager();
+		groupChatFragment = (GroupChatMessagingFragment) fm.findFragmentByTag(GROUP_CHAT_FRAGMENT);
+
+	    // If the Fragment is non-null, then it is currently being
+	    // retained across a configuration change.
+		if (groupChatFragment == null) {
+			groupChatFragment = new GroupChatMessagingFragment();
+			groupChatFragment.setArguments(extras);
+			fm.beginTransaction().add(R.id.fragmentContainer, groupChatFragment, GROUP_CHAT_FRAGMENT).commit();
+	    }
+	}
+}
---
+++ group-chat-src/org/linphone/groupchat/ui/GroupChatMessagingFragment.java
@@ -0,0 +1,127 @@
+package org.linphone.groupchat.ui;
+
+import org.linphone.ChatFragment;
+import org.linphone.core.LinphoneChatMessage;
+import org.linphone.groupchat.core.LinphoneGroupChatRoom;
+import org.linphone.ui.BubbleChat;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class GroupChatMessagingFragment extends ChatFragment
+{
+	private static GroupChatMessagingFragment instance;		// Not in documentation
+	
+	private TextView groupName;
+	private ImageView groupPicture;
+	private TextView remoteMemberComposing;
+	
+	private LinphoneGroupChatRoom chatroom;
+	private GroupChatMessageAdapter groupChatMessageAdapter;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		instance = this;
+		return null;
+		
+	}
+	
+	public void onPause()
+	{
+		
+		
+	}
+	
+	public void onResume()
+	{
+		
+		
+	}
+	
+	
+	public static GroupChatMessagingFragment instance() 
+	{
+		return instance;
+	}
+	
+	private void displayChatHeader(String displayName, String pictureUri) 
+	{
+		
+
+	}
+	
+	private void sendImageMessage(String path) 
+	{
+		
+		
+	}
+	
+	private void sendTextMessage(String messageToSend) 
+	{
+		
+		
+		
+	}
+	
+	public class GroupChatMessageAdapter extends BaseAdapter
+	{
+		LinphoneChatMessage[] history;
+		Context context;
+		
+		public GroupChatMessageAdapter(Context context, LinphoneChatMessage[] history) 
+		{
+			this.history = history;
+			this.context = context;
+		}
+		
+		public void refreshHistory()
+		{
+			
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent)
+		{
+			LinphoneChatMessage message = history[position];
+			
+			BubbleChat bubble = new BubbleChat(context, message, GroupChatMessagingFragment.this);
+			View v = bubble.getView();
+			
+			//registerForContextMenu(v);
+			RelativeLayout rlayout = new RelativeLayout(context);
+			rlayout.addView(v);
+			
+			return rlayout;
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		
+	}
+}
