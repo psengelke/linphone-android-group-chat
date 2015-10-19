fb41cc6182627972b1dcbdfd1a52d1c25969318d
Prenolan Govender <u13102380@tuks.co.za>
2015-10-19 15:31:56 +0200

    Encryption

--- group-chat-src/org/linphone/groupchat/encryption/EncryptionSeedGenerator.java
+++ group-chat-src/org/linphone/groupchat/encryption/EncryptionSeedGenerator.java
@@ -8,14 +8,20 @@ import android.util.Log;
 
 class EncryptionSeedGenerator {
 	
-	public static SecureRandom random = new SecureRandom();
+//	public static SecureRandom random = new SecureRandom();
 	
 	public static String generateSeed(int bitLength) {
 		
-		BigInteger r = new BigInteger(bitLength, random);
+		/*BigInteger r = new BigInteger(bitLength, random);
 		
 		Log.e("generateSeed", r.toString(32)); // check string
 		
-		return r.toString(32);
+		return r.toString(32);*/
+		
+		Random rng=new Random();
+		byte[] r = new byte[bitLength];
+		rng.nextBytes(r);
+		Log.e("generateSeed", r.toString()); // check string
+		return new String(r);
 	}
 }
